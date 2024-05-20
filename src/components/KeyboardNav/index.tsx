afedc61a18c046b0778ed58428d4544674da4a00
Vitor Onofre Ramos <v1toronofreramos@outlook.com>
2024-05-20 09:03:50 -0300

    Teste 1

--- src/components/KeyboardNav/index.tsx
+++ src/components/KeyboardNav/index.tsx
@@ -2,6 +2,9 @@
 
 import React, { useEffect, useRef } from 'react';
 
+// Definindo um tipo unificado para os elementos focáveis
+type FocusableElement = HTMLAnchorElement | HTMLButtonElement | HTMLInputElement;
+
 interface NavigationProps {
   children: React.ReactNode;
 }
@@ -12,21 +15,21 @@ const KeyboardNav: React.FC<NavigationProps> = ({ children }) => {
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       const activeElement = document.activeElement;
-      const elements = Array.from(refContainer.current?.querySelectorAll('a, button, input') || []);
-      const focusableElements = elements.filter(el => el!== undefined);
+      const elements = Array.from(refContainer.current?.querySelectorAll('a, button, input') || []) as FocusableElement[];
+      const focusableElements = elements.filter(el => el.focus!== undefined);
       const currentIndex = focusableElements.indexOf(activeElement);
 
       switch (event.key) {
         case 'ArrowLeft':
         case 'ArrowUp':
           if (currentIndex > 0) {
-            focusableElements[currentIndex - 1];
+            focusableElements[currentIndex - 1].focus();
           }
           break;
         case 'ArrowRight':
         case 'ArrowDown':
           if (currentIndex < focusableElements.length - 1) {
-            focusableElements[currentIndex + 1];
+            focusableElements[currentIndex + 1].focus();
           }
           break;
       }
