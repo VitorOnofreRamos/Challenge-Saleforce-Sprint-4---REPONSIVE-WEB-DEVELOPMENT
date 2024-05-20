2ea61465ad70c2433bfd3dba23435b79098af499
Vitor Onofre Ramos <v1toronofreramos@outlook.com>
2024-05-20 08:45:51 -0300

    Teste

--- src/components/KeyboardNav/index.tsx
+++ src/components/KeyboardNav/index.tsx
@@ -13,20 +13,20 @@ const KeyboardNav: React.FC<NavigationProps> = ({ children }) => {
     const handleKeyDown = (event: KeyboardEvent) => {
       const activeElement = document.activeElement;
       const elements = Array.from(refContainer.current?.querySelectorAll('a, button, input') || []);
-      const focusableElements = elements.filter(el => el.focus!== undefined);
+      const focusableElements = elements.filter(el => el!== undefined);
       const currentIndex = focusableElements.indexOf(activeElement);
 
       switch (event.key) {
         case 'ArrowLeft':
         case 'ArrowUp':
           if (currentIndex > 0) {
-            focusableElements[currentIndex - 1].focus();
+            focusableElements[currentIndex - 1];
           }
           break;
         case 'ArrowRight':
         case 'ArrowDown':
           if (currentIndex < focusableElements.length - 1) {
-            focusableElements[currentIndex + 1].focus();
+            focusableElements[currentIndex + 1];
           }
           break;
       }
